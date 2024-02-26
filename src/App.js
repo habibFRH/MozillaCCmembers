09700c68a930104574ba7f8ade1de17e87a45dc8
habibFrh <habibellah.ferhi@univ-constantone2.dz>
2024-02-26 04:04:29 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -5,13 +5,14 @@ import Latifa from "../src/assets/Latifa1.jpg";
 import Chaima from "../src/assets/Chaima.jpg";
 import Sanad from "../src/assets/saned.jpg";
 import Basmala from "../src/assets/basmala.jpg";
+import Sidou from "../src/assets/sidou.jpg";
 
 function App() {
   return (
     <div className="flex flex-col items-center">
       {/* President */}
       <u><h1 className="text-2xl mt-9 font-bold">President / Vice President</h1></u>
-      <div className="flex justify-center items-center mt-8">
+      <div className="flex justify-center items-center ">
         <div className="flex flex-col md:flex-row">
           <Card name="Roua Madoui" img={Latifa} role="President" />
           <Card name="Latifa boudiaf" img={Latifa} role="Vice President" />
@@ -19,35 +20,36 @@ function App() {
       </div>
       {/* Managers */}
       <u><h1 className="text-2xl mt-9 font-bold">Managers / Leaders</h1></u>
-      <div className="flex justify-center items-center mt-8">
+      <div className="flex justify-center items-center">
         <div className="flex flex-wrap justify-center">
+        <Card name="Moundher Mesphar" img={Asmaa} role="External Relations Manager /Artificial Intelligence-ML Leader" />
+        <Card name="Ferhi Mohamed habib Ellah" img={Sidou} role="Web Dev Leader" />
           <Card name="Asmaa" img={Asmaa} role="Education Team Manager" />
           <Card name="Chaima" img={Chaima} role="Marketing Team Manager" />
-          <Card
-            name="Soumaia Machiche"
-            img={Sanad}
-            role="Project Team Manager"
-          />
-          <Card
-            name="Soumaia Machiche"
-            img={Sanad}
-            role="Project Team Manager"
-          />
+          <Card name="Soumaia Machiche(Sanad)" img={Sanad}role="Project Team Manager"/>
         </div>
       </div>
       {/* members */}
       <u><h1 className="text-2xl mt-9 font-bold">Members</h1></u>
-      <div className="flex justify-center items-center mt-8">
+      <div className="flex justify-center items-center">
         <div className="flex flex-wrap justify-center">
           <Card name="Alice Smith" img={Asmaa} role="Data Scientist" />
           <Card name="Chaima Zamouche" img={Chaima} role="UI/UX Designer" />
           <Card name="Emily Brown" img={Sanad} role="Marketing Manager" />
-          <Card
-            name="Mike Wilson"
-            img="https://via.placeholder.com/300"
-            role="Product Manager"
-          />
-          <Card name="Basmala" img={Basmala} role="Member" />
+          <Card name="Basmala Bouchouit" img={Basmala} role="Member" />
+          <Card name="Alice Smith" img={Asmaa} role="Data Scientist" />
+          <Card name="Chaima Zamouche" img={Chaima} role="UI/UX Designer" />
+          <Card name="Emily Brown" img={Sanad} role="Marketing Manager" />
+          <Card name="Basmala Bouchouit" img={Basmala} role="Member" />
+          <Card name="Alice Smith" img={Asmaa} role="Data Scientist" />
+          <Card name="Chaima Zamouche" img={Chaima} role="UI/UX Designer" />
+          <Card name="Emily Brown" img={Sanad} role="Marketing Manager" />
+          <Card name="Basmala Bouchouit" img={Basmala} role="Member" />
+          <Card name="Alice Smith" img={Asmaa} role="Data Scientist" />
+          <Card name="Chaima Zamouche" img={Chaima} role="UI/UX Designer" />
+          <Card name="Emily Brown" img={Sanad} role="Marketing Manager" />
+          <Card name="Basmala Bouchouit" img={Basmala} role="Member" />
+          <Card name="Basmala Bouchouit" img={Basmala} role="Member" />
         </div>
       </div>
     </div>
